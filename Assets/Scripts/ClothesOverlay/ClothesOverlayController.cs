f7c3ce154d698e2132122f3da9a24be1d400bc8c
Jiayao Yu <yjybxx2@yahoo.com>
2025-08-18 22:38:35 +0200

    Added a bunch of prefabs from Jiayaos part.

--- Assets/Scripts/ClothesOverlay/ClothesOverlayController.cs
+++ Assets/Scripts/ClothesOverlay/ClothesOverlayController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class OutlookController : MonoBehaviour
+public class ClothesOverlayController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
