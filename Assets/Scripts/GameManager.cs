d9e8f76e6642a0468930059d2d4852a7674ec975
Jiayao Yu <yjybxx2@yahoo.com>
2025-08-19 00:54:53 +0200

    Added clothes overlay controller reference in Game Manager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,26 @@
+using System;
+using System.Collections.Generic;
 using Oculus.Interaction.Input;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public event Action<Dictionary<UserEnvType, List<GameObject>>> ChangeClothesOverlay; 
+    
     [Header("Environment Detection Reference")]
     [SerializeField] private EnvDetectionController envDetectionController;
 
+    [Header("Clothes Overlay reference")] 
+    [SerializeField] private List<GameObject> officClothesOverlayA;
+    [SerializeField] private List<GameObject> officClothesOverlayB;
+
+    
     [Header("Debug Reference")]
     [SerializeField] private Hand leftHand;
 
     private void Start()
     {
-        envDetectionController.OnUserEnvChanged += ChangeUserOutlook;
+        envDetectionController.OnUserEnvChanged += ChangeClothesOverlayWrapper;
     }
 
     private void Update()
@@ -20,8 +29,18 @@ public class GameManager : MonoBehaviour
             envDetectionController.ChangeUserEnv();
     }
 
-    private void ChangeUserOutlook(UserEnvType envType)
+    private void ChangeClothesOverlayWrapper(UserEnvType envType)
     {
         Debug.Log("~~~ detected user env type: " + envType);
+        GetClothesOverlay(envType);
+    }
+
+    private void GetClothesOverlay(UserEnvType envType)
+    {
+        // TODO: get clothes prefab according to given envType 
+        List<GameObject> chosenClothesOverlay = officClothesOverlayA;
+
+        ChangeClothesOverlay?.Invoke(new Dictionary<UserEnvType, List<GameObject>>()
+            { { envType, chosenClothesOverlay } });
     }
 }
